407091226b151e0dc243ad6db868fa7dddfe9f78
ANGELA <PR760817@gmail.com>
2026-08-28 10:26:33 -0500

    correccion 28/08/26

--- pr_empleate/settings.py
+++ pr_empleate/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'app_usuario',
     'empresas_app',
     'categorias_app',
+    'app_empleate',
 ]
 
 MIDDLEWARE = [
--- pr_empleate/urls.py
+++ pr_empleate/urls.py
@@ -2,17 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Panel de administración de Django
     path('admin/', admin.site.urls),
-
-    # Autenticación, registro y página de inicio (raíz del sitio)
-    path('', include('app_usuario.urls')),
-
-    # Rutas relativas para los demás módulos
+    path('', include('app_usuario.urls')),  # Carga la app de usuario desde la raíz
+    path('vacantes/', include('app_empleate.urls')),
     path('empresas/', include('empresas_app.urls')),
     path('categorias/', include('categorias_app.urls')),
     path('crear-empleate/', include('app_hojaVida.urls')),
     path('postulaciones/', include('app_postulacion.urls')),
     path('feedback/', include('app_feedback.urls')),
-    path('vacantes/', include('app_vacantes.urls')),
 ]
